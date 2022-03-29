9c7fd1e33ff0c5bfa95d2971b33ed2826480948f
Justus Deitert <mail@justusdeitert.de>
2022-03-29 11:25:15 +0200

    Add color configurations and Prettier JS formatting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,29 @@
 // tailwind.config.js
 const colors = require("tailwindcss/colors");
-
-const customColors = require("./colors.json")
-
-
-console.log("🚀 ~ file: tailwind.config.js ~ line 7 ~ colors.gray", colors.gray)
-
-console.log("🚀 ~ file: tailwind.config.js ~ line 5 ~ customColors", customColors)
+const customColors = require("./colors.json");
 
 module.exports = {
-    content: ["./src/**/*.{js, scss}", "./views/**/*.liquid"],
-    theme: {
-        screens: {
-            sm: "480px",
-            md: "768px",
-            lg: "976px",
-            // xl: '1440px',
-        },
-        colors: {
-            transparent: "transparent",
-            current: "currentColor",
-            black: colors.black,
-            white: colors.white,
-            gray: colors.gray,
-            indigo: colors.indigo,
-            red: colors.rose,
-            yellow: colors.amber,
-            acapulco: customColors.acapulco
-        },
-    },
-    plugins: [],
+	content: ["./src/**/*.{js, scss}", "./views/**/*.liquid"],
+	theme: {
+		screens: {
+			sm: "480px",
+			md: "768px",
+			lg: "976px",
+			// xl: '1440px',
+		},
+		colors: {
+			transparent: "transparent",
+			current: "currentColor",
+			black: colors.black,
+			white: colors.white,
+			// gray: colors.gray,
+			indigo: colors.indigo,
+			red: colors.rose,
+			yellow: colors.amber,
+			// Custom Colors
+			acapulco: customColors.acapulco,
+			gray: customColors.gray,
+		},
+	},
+	plugins: [],
 };
