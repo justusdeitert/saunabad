b408ce835e37e538509c125e0d76a46213f1fda4
Justus Deitert <mail@justusdeitert.de>
2022-04-21 16:46:32 +0000

    chore: use SCSS variables in postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -2,11 +2,13 @@ const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
 	syntax: "postcss-scss",
-	plugins: {
-		tailwindcss: {},
-		autoprefixer: {},
-		cssnano: isProd && {
-			preset: "default",
-		},
-	},
+	plugins: [
+		require("tailwindcss"),
+		require("autoprefixer"),
+		require("@csstools/postcss-sass"),
+		isProd &&
+			require("cssnano")({
+				preset: "default",
+			}),
+	],
 };
