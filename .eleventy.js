af42080f0383e8ddf85c31fac8ae20f42a79c9f4
Justus Deitert <mail@justusdeitert.de>
2022-04-22 10:48:57 +0200

    Implement 11ty image shortcode placeholder

--- .eleventy.js
+++ .eleventy.js
@@ -1,53 +1,58 @@
 const htmlmin = require("./utils/htmlmin");
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 const Image = require("@11ty/eleventy-img");
+const fs = require("fs");
 const isProd = process.env.NODE_ENV === "production";
 
 // https://www.11ty.dev/docs/plugins/image/
 async function imageShortcode(src, alt, sizes, classes) {
-    let metadata = await Image(src, {
-        widths: [300, 600],
-        formats: ["avif", "jpeg"],
-        urlPath: "/images/",
-        outputDir: "./dist/images/",
-    });
+	if (!fs.existsSync(src)) {
+		src = "./src/images/placeholder.jpg";
+	}
 
-    let imageAttributes = {
-        class: classes,
-        alt,
-        sizes,
-        loading: "lazy",
-        decoding: "async",
-    };
+	let metadata = await Image(src, {
+		widths: [300, 600],
+		formats: ["avif", "jpeg"],
+		urlPath: "/images/",
+		outputDir: "./dist/images/",
+	});
 
-    // You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
-    return Image.generateHTML(metadata, imageAttributes);
+	let imageAttributes = {
+		class: classes,
+		alt,
+		sizes,
+		loading: "lazy",
+		decoding: "async",
+	};
+
+	// You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
+	return Image.generateHTML(metadata, imageAttributes);
 }
 
 module.exports = function (eleventyConfig) {
-    // https://www.11ty.dev/docs/shortcodes/#universal-shortcodes
-    eleventyConfig.addShortcode("hash", () => Date.now());
+	// https://www.11ty.dev/docs/shortcodes/#universal-shortcodes
+	eleventyConfig.addShortcode("hash", () => Date.now());
 
-    // https://www.11ty.dev/docs/config/#transforms-example-minify-html-output
-    if (isProd) {
-        eleventyConfig.addTransform("htmlmin", htmlmin);
-    }
+	// https://www.11ty.dev/docs/config/#transforms-example-minify-html-output
+	if (isProd) {
+		eleventyConfig.addTransform("htmlmin", htmlmin);
+	}
 
-    // https://www.11ty.dev/docs/watch-serve/#add-your-own-watch-targets
-    eleventyConfig.addWatchTarget("./src/");
+	// https://www.11ty.dev/docs/watch-serve/#add-your-own-watch-targets
+	eleventyConfig.addWatchTarget("./src/");
 
-    // https://www.11ty.dev/docs/plugins/navigation/
-    eleventyConfig.addPlugin(eleventyNavigationPlugin);
+	// https://www.11ty.dev/docs/plugins/navigation/
+	eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
-    // https://www.11ty.dev/docs/plugins/image/
-    eleventyConfig.addLiquidShortcode("image", imageShortcode);
+	// https://www.11ty.dev/docs/plugins/image/
+	eleventyConfig.addLiquidShortcode("image", imageShortcode);
 
-    return {
-        dir: {
-            input: "views",
-            output: "dist",
-            layouts: "layouts",
-            includes: "includes",
-        },
-    };
+	return {
+		dir: {
+			input: "views",
+			output: "dist",
+			layouts: "layouts",
+			includes: "includes",
+		},
+	};
 };
