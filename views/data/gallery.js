560eaac31da5b7b4ff82ab1823ec899d5527d7f8
Justus Deitert <mail@justusdeitert.de>
2022-04-30 08:51:20 +0000

    chore: configure prettier for single quotes

--- views/data/gallery.js
+++ views/data/gallery.js
@@ -1,18 +1,21 @@
 module.exports = [
 	{
-		title: "Saunagarten",
-		description: "Nach dem Saunagang können Sie in unserem Saunagarten frische Luft atmen und barfuß auf den verschiedenen Untergründen laufen.",
-		image: "./src/images/saungarten.jpg",
+		title: 'Saunagarten',
+		description:
+			'Nach dem Saunagang können Sie in unserem Saunagarten frische Luft atmen und barfuß auf den verschiedenen Untergründen laufen.',
+		image: './src/images/saungarten.jpg',
 	},
 	{
-		title: "Ruheraum",
-		description: "Ein separater Ruheraum ermöglicht Ihnen in Ruhe zu lesen oder gar den berühmten Saunaschlaf zu geniessen.",
-		image: "./src/images/ruheraum.jpg",
+		title: 'Ruheraum',
+		description:
+			'Ein separater Ruheraum ermöglicht Ihnen in Ruhe zu lesen oder gar den berühmten Saunaschlaf zu geniessen.',
+		image: './src/images/ruheraum.jpg',
 	},
 	{
-		title: "Lesesaal",
-		description: "Unsere kleine Bibliothek steht mit Bildbänden, Ausstellungskatalogen, Zeitschriften und Romanen zur Verfügung.",
-		image: "./src/images/test.jpg",
+		title: 'Lesesaal',
+		description:
+			'Unsere kleine Bibliothek steht mit Bildbänden, Ausstellungskatalogen, Zeitschriften und Romanen zur Verfügung.',
+		image: './src/images/test.jpg',
 	},
 	// {
 	// 	title: "Bar",
@@ -20,13 +23,15 @@ module.exports = [
 	// 	image: "./src/images/bar.jpg",
 	// },
 	{
-		title: "Privatsphäre",
-		description: "In unserer Anlage sind Mobiltelefone oder andere technische Geräte mit einer Video- bzw. Fotofunktion verboten. ",
-		image: "./src/images/test.jpg",
+		title: 'Privatsphäre',
+		description:
+			'In unserer Anlage sind Mobiltelefone oder andere technische Geräte mit einer Video- bzw. Fotofunktion verboten. ',
+		image: './src/images/test.jpg',
 	},
 	{
-		title: "Aufguss-Öle",
-		description: "Für unsere Aufgüsse verwenden wir ausschließlich naturreine ätherische Öle. Wie zum Beispiel Thuja, Wacholderholz oder Petitgrain.",
-		image: "./src/images/test.jpg",
+		title: 'Aufguss-Öle',
+		description:
+			'Für unsere Aufgüsse verwenden wir ausschließlich naturreine ätherische Öle. Wie zum Beispiel Thuja, Wacholderholz oder Petitgrain.',
+		image: './src/images/test.jpg',
 	},
 ];
