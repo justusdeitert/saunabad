56efa0d1800d64d391d2ec8a9b15b6ec9df4f53f
Justus Deitert <mail@justusdeitert.de>
2022-05-07 10:13:35 +0200

    Adjust gallery data and minor text changes

---
+++ views/data/gallery.js
@@ -0,0 +1,32 @@
+module.exports = [
+	{
+		title: "Saunagarten",
+		description: "Nach dem Saunagang können Sie in unserem Saunagarten frische Luft atmen und barfuß auf den verschiedenen Untergründen laufen.",
+		image: "./src/images/saungarten.jpg",
+	},
+	{
+		title: "Ruheraum",
+		description: "Ein separater Ruheraum ermöglicht Ihnen in Ruhe zu lesen oder gar den berühmten Saunaschlaf zu geniessen.",
+		image: "./src/images/ruheraum.jpg",
+	},
+	{
+		title: "Lesesaal",
+		description: "Unsere kleine Bibliothek steht mit Bildbänden, Ausstellungskatalogen, Zeitschriften und Romanen zur Verfügung.",
+		image: "./src/images/test.jpg",
+	},
+	// {
+	// 	title: "Bar",
+	// 	description: "Genießen Sie ein paar leckere Getränke an unserer Bar.",
+	// 	image: "./src/images/bar.jpg",
+	// },
+	{
+		title: "Privatsphäre",
+		description: "Technische Geräte mit einer Video- bzw. Fotofunktion sind in unserer Anlage verboten.",
+		image: "./src/images/test.jpg",
+	},
+	{
+		title: "Aufguss-Öle",
+		description: "Für unsere Aufgüsse verwednen wir ausschglie0lich naturreine ätherische Öle.Wie zum Beispiel Thuja, Wacholderholz oder Petitgrain.",
+		image: "./src/images/test.jpg",
+	},
+];
