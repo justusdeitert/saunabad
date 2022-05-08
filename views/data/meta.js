00457dc1727a7eae764b79098372493b46117a79
Justus Deitert <mail@justusdeitert.de>
2022-05-08 10:48:25 +0200

    Add site meta and sitemap.xml and adjust image filenames

---
+++ views/data/meta.js
@@ -0,0 +1,17 @@
+const title = "Saunabad Berlin - Sauna und Wellness in Prenzlauer Berg";
+const description =
+	"Unser familiäres Kiez-Saunabad befindet sich in Prenzlauer Berg. In der Blockbohlensauna erwarten Sie Aroma-Aufgüsse mit Blick in unseren Sauna-Garten.";
+const url = "https://www.saunabad-berlin.de";
+
+module.exports = {
+	description: description,
+	url: url,
+	og: {
+		title: title,
+		description: description,
+		url: url,
+		type: "image/jpeg",
+		image: "/images/rykestraße.jpg",
+		locale: "de_DE",
+	},
+};
