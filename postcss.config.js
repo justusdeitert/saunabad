c69b256bb0bdb9a4195462c55b6f98b07082ce00
justusdeitert <mail@justusdeitert.de>
2025-12-30 18:22:21 +0100

    Simplify build scripts and silence Sass deprecation warnings
    - Remove unnecessary npx prefix from eleventy commands
    - Add silenceDeprecations option to postcss-sass config
    - Refactor fluid-type mixin to use cleaner clamp() syntax

--- postcss.config.js
+++ postcss.config.js
@@ -4,7 +4,9 @@ export default {
 	syntax: 'postcss-scss',
 	plugins: [
 		(await import('postcss-import')).default,
-		(await import('@csstools/postcss-sass')).default,
+		(await import('@csstools/postcss-sass')).default({
+			silenceDeprecations: ['legacy-js-api'],
+		}),
 		(await import('@tailwindcss/postcss')).default,
 		(await import('autoprefixer')).default,
 		isProd && (await import('cssnano')).default({ preset: 'default' }),
